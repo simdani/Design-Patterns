b3caea71d99eb3b622f1312a5f90883fd3031fc0
Simonas Daniliauskas <dranxman@gmail.com>
2018-09-19 22:25:54 +0300

    add singleton pattern example from head first design patterns book

---
+++ HeadFirst/HeadFirst/SingletonPattern/ChocolateBoiler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeadFirst.SingletonPattern
+{
+    public class ChocolateBoiler
+    {
+        private bool empty;
+        private bool boiled;
+
+        private static ChocolateBoiler uniqueInstance = new ChocolateBoiler();
+
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        public static ChocolateBoiler GetInstance()
+        {
+            return uniqueInstance;
+        }
+
+        public void Fill()
+        {
+            if (IsEmpty())
+            {
+                empty = false;
+                boiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if (!IsEmpty() && IsBoiled())
+            {
+                empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!(IsEmpty() || IsBoiled()))
+            {
+                boiled = true;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return empty;
+        }
+
+        public bool IsBoiled()
+        {
+            return boiled;
+        }
+    }
+}
