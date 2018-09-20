5c4f50802630abd6790bee9d465ca6ee3165cb24
Simonas Daniliauskas <dranxman@gmail.com>
2018-09-20 19:47:38 +0300

    add simple remote control with command pattern (not final)

---
+++ HeadFirst/HeadFirst/CommandPattern/Light.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeadFirst.CommandPattern
+{
+    public class Light
+    {
+        public Light()
+        {
+        }
+
+        public void On()
+        {
+
+        }
+
+        public void Off()
+        {
+
+        }
+    }
+}
---
+++ HeadFirst/HeadFirst/CommandPattern/SimpleRemoteControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeadFirst.CommandPattern
+{
+    public class SimpleRemoteControl
+    {
+        private ICommand slot;
+
+        public SimpleRemoteControl()
+        {
+        }
+
+        public void SetCommand(ICommand command)
+        {
+            slot = command;
+        }
+
+        public void ButtonWasPressed()
+        {
+            slot.Execute();
+        }
+    }
+}
