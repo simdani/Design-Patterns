1f4e3ac5292c1ec7dee5f66a38b1950def08abbc
Simonas D <dranxman@gmail.com>
2018-11-09 20:33:20 +0200

    add state design pattern

---
+++ DPJava/src/com/company/TutorialsPoint/Strategy/Strategy/State/Context.java
@@ -0,0 +1,17 @@
+package com.company.TutorialsPoint.Strategy.Strategy.State;
+
+public class Context {
+    private State state;
+
+    public Context() {
+        state = null;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public State getState() {
+        return state;
+    }
+}
---
+++ DPJava/src/com/company/TutorialsPoint/Strategy/Strategy/State/State.java
@@ -0,0 +1,5 @@
+package com.company.TutorialsPoint.Strategy.Strategy.State;
+
+public interface State {
+    public void doAction(Context context);
+}
